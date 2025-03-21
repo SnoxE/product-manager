2b2ddc0cce82a25c2ba29694119d99cd4340ebbc
SnoxE <oicmyzs2000@gmail.com>
2025-03-21 14:57:18 +0100

    Initial commit

---
+++ src/main/java/com/example/product_manager/ProductManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.product_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductManagerApplication.class, args);
+	}
+
+}
